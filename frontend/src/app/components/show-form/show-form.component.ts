6d5a33d2e39c4462df8229fa8beab0e0d3d98fdf
lolarne <rne.lola@gmail.com>
2025-06-15 14:30:37 -0400

    intial frontend commit, added components + pages + services

---
+++ frontend/src/app/components/show-form/show-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-form',
+  imports: [],
+  templateUrl: './show-form.component.html',
+  styleUrl: './show-form.component.scss'
+})
+export class ShowFormComponent {
+
+}
---
+++ frontend/src/app/components/show-list/show-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-list',
+  imports: [],
+  templateUrl: './show-list.component.html',
+  styleUrl: './show-list.component.scss'
+})
+export class ShowListComponent {
+
+}
